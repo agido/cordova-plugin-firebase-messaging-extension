5e716b3f93780f994b0f79f02689ce836ac1c8b4
Ben Wilkes <ben.wilkes@agido.com>
2023-01-24 11:58:51 +0100

    switch to 1.1.3

--- src/android/FirebaseMessagingExtensionPlugin.java
+++ src/android/FirebaseMessagingExtensionPlugin.java
@@ -1,11 +1,13 @@
 package ca.koho.cordova.plugin.firebase.messaging;
 
+import android.annotation.SuppressLint;
 import android.util.Log;
 
 import org.apache.cordova.CordovaPlugin;
 
 public class FirebaseMessagingExtensionPlugin extends CordovaPlugin {
     private static final String TAG = "FirebaseMessagingExtensionPlugin";
+    @SuppressLint("LongLogTag")
     @Override
     protected void pluginInitialize() {
         Log.d(TAG, "pluginInitialize");
--- src/android/FirebaseMessagingExtensionService.java
+++ src/android/FirebaseMessagingExtensionService.java
@@ -1,5 +1,6 @@
 package ca.koho.cordova.plugin.firebase.messaging;
 
+import android.annotation.SuppressLint;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -13,17 +14,20 @@ public class FirebaseMessagingExtensionService extends FirebaseMessagingService 
     private final IntercomPushClient intercomPushClient = new IntercomPushClient();
     private static final String TAG = "FirebaseMessagingExtensionService";
 
+    @SuppressLint("LongLogTag")
     @Override
     public void onCreate() {
         Log.d(TAG, "onCreate");
     }
 
+    @SuppressLint("LongLogTag")
     @Override
     public void onNewToken(String token) {
         Log.d(TAG, "onNewToken");
         intercomPushClient.sendTokenToIntercom(getApplication(), token);
     }
 
+    @SuppressLint("LongLogTag")
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
        Log.d(TAG, "onMessageReceived");
